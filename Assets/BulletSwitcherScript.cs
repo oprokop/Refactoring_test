4d91c6ffd6b45a396cfabebda4e02db0b2113b64
Artem Mazgo <a.mazgo@strikerz.inc>
2022-11-22 14:31:45 +0300

    Added bullet swicher

---
+++ Assets/BulletSwitcherScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSwitcherScript : MonoBehaviour
+{
+    public GameObject bullet;
+    
+    void Start() {  }
+
+    void Update() { }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Character")
+        {
+            other.gameObject.GetComponent<Character>().bullet = bullet;
+        }
+    }
+}
