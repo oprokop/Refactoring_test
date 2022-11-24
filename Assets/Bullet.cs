50ba58b1c0a3a5c239645b9363198b78d6548fe3
Artem Mazgo <mazgoAE@gmail.com>
2022-11-24 17:47:55 +0300

    Added effects

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -5,26 +5,54 @@ using System.Linq;
 using UnityEngine;
 
 [Serializable]
-public struct EffectsKeyValue
+public struct Data
 { 
     public string Key;
-    public GameObject VFX;
+    public GameObject StartVFX;
+    public GameObject ExplosionVFX;
     public float LiveTime;
     public float ReactionTime;
 }
 
 public class Bullet : MonoBehaviour
 {
-    public List<EffectsKeyValue> effects;
+    public List<Data> Data;
 
-    void Start() { }
+    void Start() 
+    {
+        var data = Data.Single(s => gameObject.name.Contains(s.Key));
+        var effectInstance = Instantiate(data.StartVFX, transform.position, Quaternion.identity);
+        Destroy(effectInstance, effectInstance.GetComponent<ParticleSystem>().main.duration);
+
+        var timer = gameObject.AddComponent<Timer>();
+        timer.TargetTime = 3.0f;
+        timer.OnTime = () =>
+        {
+            Destroy(gameObject);
+        };
+    }
 
     void Update() { }
 
     private void OnCollisionEnter(Collision collision)
     {
-        var effect = effects.Single(s => gameObject.name.Contains(s.Key));
-        var effectInstance = Instantiate(effect.VFX, collision.GetContact(0).point, Quaternion.identity);
-        effectInstance.transform.rotation *= Quaternion.FromToRotation(effectInstance.transform.up, collision.GetContact(0).normal);
+        var data = Data.Single(s => gameObject.name.Contains(s.Key));
+
+        var timer = gameObject.AddComponent<Timer>();
+        timer.TargetTime = data.ReactionTime;
+        timer.OnTime = () => 
+        {
+            var data = Data.Single(s => gameObject.name.Contains(s.Key));
+            var effectInstance = Instantiate(data.ExplosionVFX, collision.GetContact(0).point, Quaternion.identity);
+            effectInstance.transform.rotation *= Quaternion.FromToRotation(effectInstance.transform.up, collision.GetContact(0).normal);
+            Destroy(effectInstance, effectInstance.GetComponent<ParticleSystem>().main.duration);
+        };
+
+        timer = gameObject.AddComponent<Timer>();
+        timer.TargetTime = data.LiveTime;
+        timer.OnTime = () =>
+        {
+            Destroy(gameObject);
+        };
     }
 }
